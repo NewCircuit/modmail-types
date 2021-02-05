04aa616bb0d63886b15874a035af6022222bf4d7
Dylan Hackworth <dylhack@outlook.com>
2021-02-05 09:44:55 -0600

    Refactored Worker Types

--- src/srvtypes.ts
+++ src/srvtypes.ts
@@ -1,11 +1,22 @@
-export type ServerMessage = {
-  id: string;
-  task: string;
-  args: string[];
+export const WORKER_CALLS = {
+  getRoles: 'get_member_roles',
 }
 
-export type ServerResponse = {
+export interface ServerMessage {
+  id: string;
+  task: string;
+  args: any[];
+}
+
+export interface ServerResponse {
   id: string;
   data: any;
 }
 
+export interface GetRolesReq extends ServerMessage {
+  args: [string, string];
+}
+
+export interface GetRolesRes extends ServerResponse {
+  data: string[];
+}
